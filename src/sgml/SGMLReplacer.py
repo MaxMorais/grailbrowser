c51104b4ce7d65e246284b8dad50b7d59c2bc152
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2010-07-11 03:49:14 +0100

    redo sgml lexer

--- src/sgml/SGMLReplacer.py
+++ src/sgml/SGMLReplacer.py
@@ -1,12 +1,12 @@
 """Simple parser that handles only what's allowed in attribute values."""
 __version__ = '$Revision: 1.12 $'
 
-import regex
+import re
 import string
 from SGMLLexer import *
 
 
-_entref_exp = regex.compile("&\(\(#\|\)[a-zA-Z0-9][-.a-zA-Z0-9]*\)\(;\|\)")
+_entref_exp = regex.compile('&((#|)[a-zA-Z0-9][-.a-zA-Z0-9]*)(;|)')
 
 _named_chars = {'#re' : '\r',
                 '#rs' : '\n',
@@ -29,9 +29,14 @@ def replace(data, entities = None):
     data = string.translate(data, _chartable)
     if '&' in data and entities:
         value = None
-        pos = _entref_exp.search(data)
+        match = _entref_exp.search(data)
+        if match:
+            pos = match.start()
+        else:
+            pos = -1
         while pos >= 0 and pos + 1 < len(data):
-            ref, term = _entref_exp.group(1, 3)
+            pos = match.start()
+            ref, term = match.group(1, 3)
             if entities.has_key(ref):
                 value = entities[ref]
             elif _named_chars.has_key(string.lower(ref)):
@@ -42,5 +47,9 @@ def replace(data, entities = None):
                 value = None
             else:
                 pos = pos + len(ref) + len(term) + 1
-            pos = _entref_exp.search(data, pos)
+            match = _entref_exp.search(data)
+            if match:
+                pos = match.start()
+            else:
+                pos = -1
     return data
