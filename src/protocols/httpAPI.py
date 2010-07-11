9fe6b62cccc54b0df13b8ea6c4495b1dba53d51e
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2010-07-11 04:50:54 +0100

    use re not sre

--- src/protocols/httpAPI.py
+++ src/protocols/httpAPI.py
@@ -23,7 +23,7 @@ from Assert import Assert
 import grailutil
 import select
 import Reader
-import sre
+import re
 import StringIO
 import socket
 import sys
@@ -32,14 +32,14 @@ from __main__ import GRAILVERSION
 
 httplib.HTTP_VERSIONS_ACCEPTED = 'HTTP/1\\.[0-9.]+'
 replypat = httplib.HTTP_VERSIONS_ACCEPTED + '[ \\t]+([0-9][0-9][0-9])(.*)'
-replyprog = sre.compile(replypat)
+replyprog = re.compile(replypat)
 
 httplib.replypat = replypat
 httplib.replyprog = replyprog
 
 
 # Search for blank line following HTTP headers
-endofheaders = sre.compile('\\n[ \\t]*\\r?\\n')
+endofheaders = re.compile('\\n[ \\t]*\\r?\\n')
 
 
 # Stages
